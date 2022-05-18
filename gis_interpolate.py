7542421a11f81f976ac6e41c3059436ce0f72a64
Dima Pestunov <pestuno@gmail.com>
2022-05-18 22:49:10 +0700

    puts all data in grid with specified steps

--- gis_interpolate.py
+++ gis_interpolate.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 16:39:00 2022
+
+@author: Phil
+"""
+
 import pandas as pd
 import numpy as np
 import matplotlib as mpl
@@ -5,24 +12,46 @@ import matplotlib.pyplot as plt
 
 plt.style.use('seaborn-whitegrid')   # try another styles: 'classic'
 
-NDOTS = 100
+srcData = "D:/1_Data1/SeaAllMethane/methaneSea.dat"
+destData = "D:/1_Data1/SeaAllMethane/methaneSeaRes.dat"
+# srcData = "~/methaneSea.dat"
 
-#lat = [1, 2, 3, 3.2, 3.4, 1.2, 2.1, 4.1]
-#long = [1, 2, 3, 5, 4.5, 2.2, 3.1, 0.1]
-#param = [2, 3, 2, 1, 3, 2.3, 2.6, 1.2]
-x = np.linspace(1, 10, 100)
-y = np.linspace(1, 10, 100)
-X, Y = np.meshgrid(x, y)
-Z = 1/X + np.sin(X)/Y 
+df = pd.read_csv(srcData,
+                 header=1,
+                 #skip_blank_lines=True,
+                 skipinitialspace=True,
+                 na_values='--',
+                 skiprows=[2],
+                 sep='\t',
+                 decimal=',',
+                 #nrows=100000,
+                 usecols=[1,2,3],
+                 )
+
+
+longStep = 0.01
+latStep = 0.01
+
+df['long'] = (df['longitude']/longStep).round()*longStep
+df['lat'] = (df['latitude']/latStep).round()*latStep
+
+res = df.groupby(['long', 'lat'], as_index=False).agg({'ch4_water_calc': ['mean', 'count']})
+
+res.to_csv(destData,
+           sep='\t',
+           )
+
+valMax = res['ch4_water_calc'].max
+
 
 fig = plt.figure()
 
 # wether this or those
-# plt.scatter(lat, long, c=param, s=100, marker='s')
-# plt.colorbar()
-
-plt.contourf(X, Y, Z, 20, cmap='RdGy')
+plt.scatter(res['long'], res['lat'], c='r', s=10, marker='s')
 
 
 fig.show()
-fig.savefig('/home/pi/pic.png')
+fig.savefig('gis.png')
+
+print(df)
+print(res)
